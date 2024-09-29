cc47716ce2efe606bd836c37efb1de8c63195042
Brian Yang <brianyang120488@gmail.com>
2024-09-29 15:08:02 -0400

    Initial commit

---
+++ FileCopy/fileclient.cpp
@@ -0,0 +1,202 @@
+#include <string>
+#include <stdlib.h>
+#include "c150grading.h"
+#include <filesystem>
+#include "c150nastyfile.h"
+#include <fstream> 
+#include <openssl/sha.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include "c150dgmsocket.h"
+
+
+using namespace std;
+namespace fs = filesystem;
+using namespace C150NETWORK;
+
+
+string make_hash(string file_name);
+void print_hash(const string& obuf);
+ssize_t write_to_server_and_wait(C150DgmSocket *sock, string message, char *incomingMessage);
+
+void checkAndPrintMessage(ssize_t readlen, char *msg, ssize_t bufferlen);
+
+int main(int argc, char *argv[]) {
+
+    //
+    //  DO THIS FIRST OR YOUR ASSIGNMENT WON'T BE GRADED!
+    //
+  
+    // GRADEME(argc, argv);
+
+    if (argc != 5) {
+        fprintf(stderr,"Correct syntax is %s <server> <networknastiness> <filenastiness> <srcdir>\n", argv[0]);
+        exit (4);
+    }
+
+    if (strspn(argv[2], "0123456789") != strlen(argv[2]) || strspn(argv[3], "0123456789") != strlen(argv[3])) {
+        fprintf(stderr,"Nastiness %s or %s is not numeric\n", argv[2], argv[3]);    
+        exit(4);
+    }
+
+    char* server = argv[1];
+    // int network_nastiness = atoi(argv[2]);
+    // int file_nastiness = atoi(argv[3]);
+    char* srcdir = argv[4];
+
+    // example for gradelog output
+    // *GRADING << "File: <name>, beginning transmission, attempt <attempt>" endl;
+    try {
+        for (const auto& entry : fs::directory_iterator(srcdir)) {
+            string file_name = entry.path().filename().string();
+            string hash = make_hash(entry.path().string());
+  
+            C150DgmSocket *sock = new C150DgmSocket();
+            sock -> setServerName(server);  
+            sock -> turnOnTimeouts(1000);
+
+            char incomingMessage[512];
+            ssize_t readlen;
+            try {
+                readlen = write_to_server_and_wait(sock, "CHECK " + file_name, incomingMessage);
+                string incoming(incomingMessage);
+                size_t spacePos = incoming.find(' ');
+                string typeOfRead = incoming.substr(0, spacePos);
+                string incoming_hash = incoming.substr(spacePos + 1);
+
+                checkAndPrintMessage(readlen, incomingMessage, 512);
+                if (incoming_hash == hash) {
+                    cout << "THE SAME!" << endl;
+                    // readlen = write_to_server_and_wait(sock, "THE SAME!", incomingMessage);
+                    // checkAndPrintMessage(readlen, incomingMessage, 512);
+                }
+                else{
+                    cout << "NOT THE SAME" << endl;
+                    // readlen = write_to_server_and_wait(sock, "NOT THE SAME!", incomingMessage);
+                    // checkAndPrintMessage(readlen, incomingMessage, sizeof(incomingMessage));
+                }
+            }
+            catch (C150NetworkException& e) {
+                c150debug->printf(C150ALWAYSLOG,"Caught C150NetworkException: %s\n",
+                                e.formattedExplanation().c_str());
+                cerr << argv[0] << ": caught C150NetworkException: " << e.formattedExplanation()
+                                << endl;
+            }
+            cout << endl;
+        }
+    }
+    catch (const fs::filesystem_error& e) {
+        cerr << "Filesystem error: " << e.what() << endl;
+    } catch (const exception& e) {
+        cerr << "General exception: " << e.what() << endl;
+    }
+    
+    return 0;
+}
+
+
+ssize_t write_to_server_and_wait(C150DgmSocket *sock, string message, char *incomingMessage) {
+    ssize_t readlen;
+    int maxRetries = 5;
+    bool messageReceived = false;
+    for (int retries = 0; retries < maxRetries && !messageReceived; retries++) {
+        sock->write(message.c_str(), message.length() + 1); // +1 includes the null terminator
+        readlen = sock->read(incomingMessage, 512);
+
+        if (!sock->timedout()) {
+            messageReceived = true;
+        }
+
+        // TODO: contineu reading for packets?
+    }
+    return readlen;
+}
+
+string make_hash(string file_path) {
+    // Open the file
+    std::ifstream t(file_path, std::ios::binary);
+    if (!t.is_open()) {
+        throw std::runtime_error("Unable to open file: " + file_path);
+    }
+
+    // Read the entire file content into a string
+    std::stringstream buffer;
+    buffer << t.rdbuf();
+    std::string content = buffer.str();
+
+    // Compute the SHA-1 hash
+    unsigned char hash[SHA_DIGEST_LENGTH];
+    SHA1(reinterpret_cast<const unsigned char*>(content.c_str()), content.length(), hash);
+
+    // Convert the binary hash to a hex string
+    std::stringstream hex_stream;
+    for (int i = 0; i < SHA_DIGEST_LENGTH; ++i) {
+        hex_stream << std::hex << std::setw(2) << std::setfill('0') << (int)hash[i];
+    }
+
+    // Return the hex string
+    return hex_stream.str();
+}
+
+void print_hash(const string& obuf) {
+    for (size_t i = 0; i < obuf.size(); i++) {
+        printf("%02x", (unsigned char)obuf[i]);
+    }
+    cout << endl;
+}
+
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+//
+//                     checkAndPrintMessage
+//
+//        Make sure length is OK, clean up response buffer
+//        and print it to standard output.
+//
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+ 
+
+
+void checkAndPrintMessage(ssize_t readlen, char *msg, ssize_t bufferlen) {
+    // 
+    // Except in case of timeouts, we're not expecting
+    // a zero length read
+    //
+    if (readlen == 0) {
+        throw C150NetworkException("Unexpected zero length read in client");
+    }
+
+    // DEFENSIVE PROGRAMMING: we aren't even trying to read this much
+    // We're just being extra careful to check this
+    if (readlen > (int)(bufferlen)) {
+        throw C150NetworkException("Unexpected over length read in client");
+    }
+
+    //
+    // Make sure server followed the rules and
+    // sent a null-terminated string (well, we could
+    // check that it's all legal characters, but 
+    // at least we look for the null)
+    //
+    if(msg[readlen-1] != '\0') {
+        throw C150NetworkException("Client received message that was not null terminated");     
+    };
+
+    //
+    // Use a routine provided in c150utility.cpp to change any control
+    // or non-printing characters to "." (this is just defensive programming:
+    // if the server maliciously or inadvertently sent us junk characters, then we 
+    // won't send them to our terminal -- some 
+    // control characters can do nasty things!)
+    //
+    // Note: cleanString wants a C++ string, not a char*, so we make a temporary one
+    // here. Not super-fast, but this is just a demo program.
+    string s(msg);
+    cleanString(s);
+
+    // Echo the response on the console
+
+    c150debug->printf(C150APPLICATION,"PRINTING RESPONSE: Response received is \"%s\"\n", s.c_str());
+    printf("Response received is \"%s\"\n", s.c_str());
+
+}
---
+++ FileCopy/fileserver.cpp
@@ -0,0 +1,323 @@
+#include "c150nastydgmsocket.h"
+#include "c150debug.h"
+#include "c150grading.h"
+#include <fstream>
+#include <cstdlib> 
+#include <sstream> 
+#include <iomanip> 
+#include <openssl/sha.h>  // For SHA1
+
+
+using namespace std;
+using namespace C150NETWORK;
+
+string make_hash(string file_name);
+C150DgmSocket* createSocket(int nastiness);
+void checkAndPrintMessage(ssize_t readlen, char *msg, ssize_t bufferlen);
+void processIncomingMessages(C150DgmSocket *sock, const string &programName);
+bool sendMessageWithRetries(C150DgmSocket *sock, const string &msg, int maxRetries);
+
+const int maxRetries = 5;
+
+int main(int argc, char *argv[]) {
+    //
+    // Variable declarations
+    //
+    int nastiness;               // how aggressively do we drop packets, etc?
+
+    //
+    // Check command line and parse arguments
+    //
+    if (argc != 2)  {
+        fprintf(stderr,"Correct syntxt is: %s <nastiness_number>\n", argv[0]);
+        exit(1);
+    }
+    if (strspn(argv[1], "0123456789") != strlen(argv[1])) {
+        fprintf(stderr,"Nastiness %s is not numeric\n", argv[1]);     
+        fprintf(stderr,"Correct syntxt is: %s <nastiness_number>\n", argv[0]);     
+        exit(4);
+    }
+    nastiness = atoi(argv[1]);   // convert command line string to integer
+       
+    //
+    //  Set up debug message logging
+    //
+    // setUpDebugLogging("fileserverdebug.txt", argc, argv);
+
+    // c150debug->setIndent("    ");              // if we merge client and server
+    try {
+        // Create the socket
+        C150DgmSocket *sock = createSocket(nastiness);
+        processIncomingMessages(sock, argv[0]);
+
+    } catch(C150NetworkException& e) {
+         // Write to debug log
+        c150debug->printf(C150ALWAYSLOG,"Caught C150NetworkException: %s\n",
+                          e.formattedExplanation().c_str());
+        // In case we're logging to a file, write to the console too
+        cerr << argv[0] << ": caught C150NetworkException: " << e.formattedExplanation() << endl;
+    }
+
+    return 0;
+}
+
+
+// Function to create and configure the socket
+C150DgmSocket* createSocket(int nastiness) {
+    c150debug->printf(C150APPLICATION,"Creating C150NastyDgmSocket(nastiness=%d)", nastiness);
+
+    C150DgmSocket *sock = new C150NastyDgmSocket(nastiness);
+    sock->turnOnTimeouts(500);  // Set a 500ms timeout
+
+    c150debug->printf(C150APPLICATION,"Socket ready to accept messages");
+
+    return sock;
+}
+
+// Function to process incoming messages and handle responses
+void processIncomingMessages(C150DgmSocket *sock, const string &programName) {
+    cout << "Inside processIncomingMessages\n";
+    ssize_t readlen;
+    char incomingMessage[512];
+
+    while (1) {
+        readlen = sock->read(incomingMessage, sizeof(incomingMessage) - 1);
+        if (readlen == 0) {
+            c150debug->printf(C150APPLICATION,"Read zero length message, trying again");
+            continue;
+        }
+        incomingMessage[readlen] = '\0';  // Ensure null termination
+        
+        string incoming(incomingMessage);
+    
+        // Calculate the number of characters before the space
+        size_t spacePos = incoming.find(' ');
+        // Copy the characters before the space into firstToken
+        string typeOfRead = incoming.substr(0, spacePos);
+        cout << typeOfRead << endl;
+
+        string incomingFile = incoming.substr(spacePos + 1);
+
+        string filePath = "TARGET/" + string(incomingFile);
+
+        string hash = make_hash(filePath);
+
+        string msg = incomingFile + " " + hash;
+
+        sock->write(msg.c_str(), strlen(msg.c_str()) + 1); // +1 includes the null terminator
+
+        // c150debug->printf(C150APPLICATION, "Returned from write, doing read()");
+
+        // bool success = sendMessageWithRetries(sock, hash, maxRetries);
+        // if (!success) {
+        //     throw C150NetworkException("Server is not responding after multiple attempts.");
+        // }
+    }
+}
+
+// Function to send a message and handle retries
+bool sendMessageWithRetries(C150DgmSocket *sock, const string &msg, int maxRetries) {
+    ssize_t readlen;
+    char incomingMessage[512];
+    int retries = 0;
+    bool messageReceived = false;
+
+    while (retries < maxRetries && !messageReceived) {
+        c150debug->printf(C150APPLICATION, "Writing message: \"%s\"", msg.c_str());
+        sock->write(msg.c_str(), msg.length() + 1); // +1 includes the null terminator
+
+        c150debug->printf(C150APPLICATION, "Returned from write, doing read()");
+        readlen = sock->read(incomingMessage, sizeof(incomingMessage));
+
+        if (sock->timedout()) {
+            retries++;
+            c150debug->printf(C150APPLICATION, "Timeout occurred. Retrying (%i/%i)...\n", retries, maxRetries);
+        } else {
+            messageReceived = true;
+            checkAndPrintMessage(readlen, incomingMessage, sizeof(incomingMessage));
+
+            // Keep reading additional responses until timeout
+            while (1) {
+                readlen = sock->read(incomingMessage, sizeof(incomingMessage));
+                if (sock->timedout()) {
+                    c150debug->printf(C150APPLICATION, "No more packets received, timing out.\n");
+                    break;
+                }
+                checkAndPrintMessage(readlen, incomingMessage, sizeof(incomingMessage));
+            }
+        }
+    }
+
+    return messageReceived;
+}
+
+// string print_hash(unsigned char obuf[20]) {
+//     ostringstream oss;
+//     for (int i = 0; i < 20; i++) {
+//         oss << setw(2) << setfill('0') << hex << (unsigned int)obuf[i];
+//     }
+//     return oss.str();
+// }
+
+string make_hash(string file_path) {
+    // Open the file
+    cout << "File path in hash func: " << file_path << endl;
+    std::ifstream t(file_path, std::ios::binary);
+    if (!t.is_open()) {
+        throw std::runtime_error("Unable to open file: " + file_path);
+    }
+
+    // Read the entire file content into a string
+    std::stringstream buffer;
+    buffer << t.rdbuf();
+    std::string content = buffer.str();
+
+    // Compute the SHA-1 hash
+    unsigned char hash[SHA_DIGEST_LENGTH];
+    SHA1(reinterpret_cast<const unsigned char*>(content.c_str()), content.length(), hash);
+
+    // Convert the binary hash to a hex string
+    std::stringstream hex_stream;
+    for (int i = 0; i < SHA_DIGEST_LENGTH; ++i) {
+        hex_stream << std::hex << std::setw(2) << std::setfill('0') << (int)hash[i];
+    }
+
+    // Return the hex string
+    return hex_stream.str();
+}
+
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+//
+//                     checkAndPrintMessage
+//
+//        Make sure length is OK, clean up response buffer
+//        and print it to standard output.
+//
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+ 
+
+
+void
+checkAndPrintMessage(ssize_t readlen, char *msg, ssize_t bufferlen) {
+    // 
+    // Except in case of timeouts, we're not expecting
+    // a zero length read
+    //
+    if (readlen == 0) {
+        throw C150NetworkException("Unexpected zero length read in client");
+    }
+
+    // DEFENSIVE PROGRAMMING: we aren't even trying to read this much
+    // We're just being extra careful to check this
+    if (readlen > (int)(bufferlen)) {
+        throw C150NetworkException("Unexpected over length read in client");
+    }
+
+    //
+    // Make sure server followed the rules and
+    // sent a null-terminated string (well, we could
+    // check that it's all legal characters, but 
+    // at least we look for the null)
+    //
+    if(msg[readlen-1] != '\0') {
+        throw C150NetworkException("Client received message that was not null terminated");     
+    };
+
+    //
+    // Use a routine provided in c150utility.cpp to change any control
+    // or non-printing characters to "." (this is just defensive programming:
+    // if the server maliciously or inadvertently sent us junk characters, then we 
+    // won't send them to our terminal -- some 
+    // control characters can do nasty things!)
+    //
+    // Note: cleanString wants a C++ string, not a char*, so we make a temporary one
+    // here. Not super-fast, but this is just a demo program.
+    string s(msg);
+    cleanString(s);
+
+    // Echo the response on the console
+
+    c150debug->printf(C150APPLICATION,"PRINTING RESPONSE: Response received is \"%s\"\n", s.c_str());
+    printf("Response received is \"%s\"\n", s.c_str());
+
+}
+
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+//
+//                     setUpDebugLogging
+//
+//        For COMP 150-IDS, a set of standards utilities
+//        are provided for logging timestamped debug messages.
+//        You can use them to write your own messages, but 
+//        more importantly, the communication libraries provided
+//        to you will write into the same logs.
+//
+//        As shown below, you can use the enableLogging
+//        method to choose which classes of messages will show up:
+//        You may want to turn on a lot for some debugging, then
+//        turn off some when it gets too noisy and your core code is
+//        working. You can also make up and use your own flags
+//        to create different classes of debug output within your
+//        application code
+//
+//        NEEDSWORK: should be factored into shared code w/pingserver
+//        NEEDSWORK: document arguments
+//
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+ 
+void setUpDebugLogging(const char *logname, int argc, char *argv[]) {
+
+    //   
+    //           Choose where debug output should go
+    //
+    // The default is that debug output goes to cerr.
+    //
+    // Uncomment the following three lines to direct
+    // debug output to a file. Comment them
+    // to default to the console.
+    //
+    // Note: the new DebugStream and ofstream MUST live after we return
+    // from setUpDebugLogging, so we have to allocate
+    // them dynamically.
+    //
+    //
+    // Explanation: 
+    // 
+    //     The first line is ordinary C++ to open a file
+    //     as an output stream.
+    //
+    //     The second line wraps that will all the services
+    //     of a comp 150-IDS debug stream, and names that filestreamp.
+    //
+    //     The third line replaces the global variable c150debug
+    //     and sets it to point to the new debugstream. Since c150debug
+    //     is what all the c150 debug routines use to find the debug stream,
+    //     you've now effectively overridden the default.
+    //
+    ofstream *outstreamp = new ofstream(logname);
+    DebugStream *filestreamp = new DebugStream(outstreamp);
+    DebugStream::setDefaultLogger(filestreamp);
+
+    //
+    //  Put the program name and a timestamp on each line of the debug log.
+    //
+    c150debug->setPrefix(argv[0]);
+    c150debug->enableTimestamp(); 
+
+    //
+    // Ask to receive all classes of debug message
+    //
+    // See c150debug.h for other classes you can enable. To get more than
+    // one class, you can or (|) the flags together and pass the combined
+    // mask to c150debug -> enableLogging 
+    //
+    // By the way, the default is to disable all output except for
+    // messages written with the C150ALWAYSLOG flag. Those are typically
+    // used only for things like fatal errors. So, the default is
+    // for the system to run quietly without producing debug output.
+    //
+    c150debug->enableLogging(C150APPLICATION | C150NETWORKTRAFFIC | 
+                             C150NETWORKDELIVERY); 
+}
